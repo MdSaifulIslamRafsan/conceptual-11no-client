cdb8c00f0b052216767dcbb0d34a9fb5a7a0cd5f
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-08 22:56:17 +0600

    added onAuthStateChanged

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,15 +1,30 @@
-import { createContext } from "react";
-
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../Firebase/firebase.config";
+import PropTypes from 'prop-types';
 export const AuthContext = createContext(null);
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+        setUser(currentUser);
+        setLoading(false);
+    })
+    return ()=>{
+        unsubscribe()
+    }
+  }, []);
 
-    const userInfo = {}
-    return (
-        <AuthContext.Provider value={userInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const userInfo = {user , loading};
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
+AuthProvider.propTypes = {
+    children: PropTypes.object,
+    
+}
